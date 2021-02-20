d5369d1d35ac176f131a209ddf88f60aed8c4685
Krys1ek <78645442+Krys1ek@users.noreply.github.com>
2021-02-20 13:09:28 +0100

    dodany GameManager
    manager obsługuje czas koniec gry i czy wygralismy

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraController : MonoBehaviour
+{
+    public float mouseSencivity = 100f;
+    public Transform playerBody = null;
+    public float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    
+    void Update()
+    {
+        playerBody = transform.parent;
+
+        float MouseX = Input.GetAxis("Mouse X") * mouseSencivity * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * mouseSencivity * Time.deltaTime;
+
+        xRotation -= MouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
+
+        playerBody.Rotate(Vector3.up * MouseX);
+    }
+}
---
+++ Assets/Scripts/Player/Manages/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager { get; private set; }
+    public int TimeToEnd = 240;
+    private bool gamePause = false;
+    private bool EndGame = false;
+    private bool win = false;
+
+    
+
+
+    private void Awake() 
+    {
+        if (gameManager == null)
+            gameManager = this;
+    }
+    public void Endgame()
+    {
+        CancelInvoke("Stopper");
+        if (win) {
+            Debug.Log("You Win! Reload?");
+        }
+        else {
+            Debug.Log("You Loose! Reload?");
+        }
+            
+    }
+    public void GamePause()
+    {
+        Debug.Log("GamePaused");
+         gamePause = true;
+        Time.timeScale = 0f;
+    }
+    public void ResumeGame()
+    {
+        Debug.Log("GameResumed");
+        gamePause = false;
+        Time.timeScale = 1.0f;
+    }
+    public void Stopper() {
+
+        TimeToEnd--;
+        Debug.Log("Time:" + TimeToEnd + "s");
+        if (TimeToEnd <= 0) {
+            TimeToEnd = 0;
+            EndGame = true;
+        }
+        if (EndGame)
+        {
+            Endgame();
+        }
+    }
+    void Start() {
+        InvokeRepeating("Stopper", 1, 1);
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (gamePause == true)
+                ResumeGame();
+            else
+                GamePause();  
+
+        }
+    }
+}
